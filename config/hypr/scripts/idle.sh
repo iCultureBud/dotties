d87ac7c3e54be55cea6c7feb3374545f5c8ae508
iCulture <99724871+iCultureBud@users.noreply.github.com>
2024-11-26 13:59:03 +0100

    update hyprland config with new windowrules and keybindings

--- config/hypr/scripts/idle.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/sh
-
-# Lock screen, when idle
-swayidle -w \
-    timeout 600 '~/.config/hypr/scripts/lock.sh -f' \
-	before-sleep 'pgrep swaylock || ~/.config/hypr/scripts/lock.sh -f && true'
--- config/hypr/scripts/startup.sh
+++ config/hypr/scripts/startup.sh
@@ -7,21 +7,9 @@ hash dbus-update-activation-environment 2>/dev/null && \
         WAYLAND_DISPLAY XDG_CURRENT_DESKTOP
 
 # GTK Theming
-gsettings set org.gnome.desktop.interface gtk-theme Nordic-Polar
-gsettings set org.gnome.desktop.wm.preferences theme Nordic
-gsettings set org.gnome.desktop.interface icon-theme Zafiro-Nord-Black-Blue
-gsettings set org.gnome.desktop.interface cursor-theme BreezeX
-
-# Autostart for Hyprland
-hyprctl setcursor BreezeX 24 &
-hyprpaper &
-waybar &
-mako &
-wlsunset -l 46.9 -L 15.2 -t 3500 &
-wl-paste -t text --watch clipman store & remind -z -k'notify-send \"Reminder:\" \"%s\"' ~/.reminders.rem &
-# remind -z -k'notify-send \"Reminder:\" \"%s\"' ~/.reminders.rem &
-swayidle -w \
-    timeout 600 '~/.config/hypr/scripts/lock.sh -f' \
-	before-sleep 'pgrep swaylock || ~/.config/hypr/scripts/lock.sh -f && true'
+gsettings set org.gnome.desktop.interface gtk-theme Gruvbox-Dark-Medium
+gsettings set org.gnome.desktop.wm.preferences theme Gruvbox-Dark-Medium
+gsettings set org.gnome.desktop.interface icon-theme Gruvbox-Plus-Dark
+gsettings set org.gnome.desktop.interface cursor-theme Bibata-Modern-Ice
 
 exit 0
