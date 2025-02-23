6a18eb450f70aca437fee57273e3904f7512928e
iCulture <99724871+iCultureBud@users.noreply.github.com>
2025-02-23 14:00:26 +0100

    [nvim] added folding logic and origami plugin for better fold movement

--- config/nvim/lua/iculture/core/options.lua
+++ config/nvim/lua/iculture/core/options.lua
@@ -1,6 +1,6 @@
 vim.cmd([[
 	set path+=**
-	set cc=120
+	set cc=100
 	set wildignore+=**/node_modules/*
 	set wildignore+=**/.git/*
 	set wildignore+=*.log*
@@ -8,6 +8,7 @@ vim.cmd([[
 	set tabstop=4
 	set softtabstop=4
 	set shiftwidth=4
+	set expandtab
 	set scrolloff=8
 	set relativenumber nu
 	set wrap
@@ -28,7 +29,16 @@ vim.cmd([[
 	set undodir=~/.local/share/undo
 	set mouse=a
 	set iskeyword=@,48-57,_,192-255,$
-	autocmd FileType * setlocal formatoptions-=c formatoptions-=r formatoptions-=o
-	autocmd FileType json setlocal expandtab
+	autocmd FileType * setlocal expandtab
 	filetype plugin indent on
 ]])
+
+vim.o.foldcolumn = 'auto'
+vim.o.foldmethod = 'expr'
+vim.o.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+vim.o.foldtext = require('iculture.core.folds')
+vim.o.foldlevel = 99
+vim.o.foldnestmax = 4
+vim.o.foldminlines = 5
+vim.o.foldlevelstart = 0
+vim.o.fillchars = 'eob: ,fold: ,foldopen:,foldsep: ,foldclose:'
