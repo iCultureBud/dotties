7b86a3952ece593c4de102050593e5e637516956
Dominik Karner <d.karner@axis-simulation.com>
2025-04-22 13:52:26 +0200

    [nvim] replace barbecue with dropbar, fix colors for winbar

--- config/nvim/lua/iculture/plugins/colors.lua
+++ config/nvim/lua/iculture/plugins/colors.lua
@@ -1,30 +1,30 @@
 return {
-	'iCultureBud/gruvbox-material.nvim',
-	priority = 1000,
-	config = function()
-		require('gruvbox-material').setup({
-			contrast = 'medium',
-			background = {
-				transparent = true,
-			}
-		})
+    'iCultureBud/gruvbox-material.nvim',
+    priority = 1000,
+    config = function()
+        require('gruvbox-material').setup({
+            contrast = 'medium',
+            background = {
+                transparent = true,
+            }
+        })
 
 
-		-- Set theme global
-		-- vim.api.nvim_command('colorscheme gruvbox-material')
+        -- Set theme global
+        -- vim.api.nvim_command('colorscheme gruvbox-material')
 
-		vim.api.nvim_set_hl(0, 'GitSignsAdd', { bg = 'none' })
-		vim.api.nvim_set_hl(0, 'GitSignsChange', { bg = 'none' })
-		vim.api.nvim_set_hl(0, 'GitSignsDelete', { bg = 'none' })
+        vim.api.nvim_set_hl(0, 'GitSignsAdd', { bg = 'none' })
+        vim.api.nvim_set_hl(0, 'GitSignsChange', { bg = 'none' })
+        vim.api.nvim_set_hl(0, 'GitSignsDelete', { bg = 'none' })
 
-		-- Set colors for indentation
-		local colors = require('gruvbox-material.colors').get(vim.o.background, 'medium')
-		vim.api.nvim_set_hl(0, 'IndentBlanklineIndent1', { fg = colors.aqua })
-		vim.api.nvim_set_hl(0, 'IndentBlanklineIndent2', { fg = colors.yellow })
-		vim.api.nvim_set_hl(0, 'IndentBlanklineIndent3', { fg = colors.blue })
-		vim.api.nvim_set_hl(0, 'IndentBlanklineIndent4', { fg = colors.red })
-		vim.api.nvim_set_hl(0, 'IndentBlanklineIndent5', { fg = colors.green })
-		vim.api.nvim_set_hl(0, 'IndentBlanklineIndent6', { fg = colors.purple })
+        -- Set colors for indentation
+        local colors = require('gruvbox-material.colors').get(vim.o.background, 'medium')
+        vim.api.nvim_set_hl(0, 'IndentBlanklineIndent1', { fg = colors.aqua })
+        vim.api.nvim_set_hl(0, 'IndentBlanklineIndent2', { fg = colors.yellow })
+        vim.api.nvim_set_hl(0, 'IndentBlanklineIndent3', { fg = colors.blue })
+        vim.api.nvim_set_hl(0, 'IndentBlanklineIndent4', { fg = colors.red })
+        vim.api.nvim_set_hl(0, 'IndentBlanklineIndent5', { fg = colors.green })
+        vim.api.nvim_set_hl(0, 'IndentBlanklineIndent6', { fg = colors.purple })
 
         vim.api.nvim_set_hl(0, 'RainbowDelimiterRed', { fg = colors.red })
         vim.api.nvim_set_hl(0, 'RainbowDelimiterYellow', { fg = colors.yellow })
@@ -34,7 +34,27 @@ return {
         vim.api.nvim_set_hl(0, 'RainbowDelimiterViolet', { fg = colors.purple })
         vim.api.nvim_set_hl(0, 'RainbowDelimiterCyan', { fg = colors.aqua })
 
-		-- Set color for Codeium in [nvim-cmp]
-		vim.api.nvim_set_hl(0, 'CmpItemKindCodeium', { fg = colors.yellow })
-	end
+        -- Set color for Codeium in [nvim-cmp]
+        vim.api.nvim_set_hl(0, 'CmpItemKindCodeium', { fg = colors.yellow })
+
+        vim.api.nvim_create_autocmd({ 'UiEnter', 'ColorScheme' }, {
+            callback = function()
+                -- 1.
+                vim.cmd [[
+                    hi TabLineFill gui=nocombine
+                    hi WinBar gui=nocombine
+                ]]
+                -- 2.
+                vim.cmd [[
+                    hi TabLineFill guibg=none
+                    hi WinBar gui=none
+                ]]
+                -- 3.
+                vim.cmd [[
+                    hi! link TabLineFill Normal
+                    hi! link WinBar Normal
+                ]]
+            end
+        })
+    end
 }
