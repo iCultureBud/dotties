ad90662fa880a8e7a92801cca516afed44aac826
iCulture <99724871+iCultureBud@users.noreply.github.com>
2025-05-18 09:53:44 +0200

    [fish] installed flutter completion + udpates

---
+++ config/fish/conf.d/virtualfish-loader.fish
@@ -0,0 +1,5 @@
+set -g VIRTUALFISH_VERSION 2.5.9
+set -g VIRTUALFISH_PYTHON_EXEC /opt/homebrew/Cellar/virtualfish/2.5.9_1/libexec/bin/python
+source /opt/homebrew/Cellar/virtualfish/2.5.9_1/libexec/lib/python3.13/site-packages/virtualfish/virtual.fish
+source /opt/homebrew/Cellar/virtualfish/2.5.9_1/libexec/lib/python3.13/site-packages/virtualfish/auto_activation.fish
+emit virtualfish_did_setup_plugins
