e3e66da77ba2342d352e518b4a6457ffe46ef2a6
iCulture <99724871+iCultureBud@users.noreply.github.com>
2025-05-23 07:05:44 +0200

    use yazi for term file manager, deleted vf

--- config/fish/conf.d/virtualfish-loader.fish
+++
@@ -1,5 +0,0 @@
-set -g VIRTUALFISH_VERSION 2.5.9
-set -g VIRTUALFISH_PYTHON_EXEC /usr/bin/python
-source /usr/lib/python3.12/site-packages/virtualfish/virtual.fish
-source /usr/lib/python3.12/site-packages/virtualfish/auto_activation.fish
-emit virtualfish_did_setup_plugins
