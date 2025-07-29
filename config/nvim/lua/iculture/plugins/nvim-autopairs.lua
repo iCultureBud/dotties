87bd13a66d34c1b5a823357dfbd9e712625d768b
iCulture <99724871+iCultureBud@users.noreply.github.com>
2025-07-29 20:17:22 +0200

    [ags] delete hyprland related functions

--- config/nvim/lua/iculture/plugins/nvim-autopairs.lua
+++ config/nvim/lua/iculture/plugins/nvim-autopairs.lua
@@ -1,6 +1,6 @@
 return {
-	'windwp/nvim-autopairs',
-	event = 'InsertEnter',
-	dependencies = 'hrsh7th/nvim-cmp',
-	opts = { check_ts = true }
+    'windwp/nvim-autopairs',
+    event = 'InsertEnter',
+    dependencies = 'hrsh7th/nvim-cmp',
+    opts = { check_ts = true }
 }
